a3bdc4ca64c0244041a026c206437ada80697808
Ruth Bovell <ruthb111@outlook.com>
2024-04-08 15:34:29 +0100

    change doBinary to false

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
         System.out.println("Hello pdfs!");
         try {
-            PdfToGrey.pdfToImage("src/main/resources/testcolours.pdf", true);
+            PdfToGrey.pdfToImage("src/main/resources/testcolours.pdf", false);
             PdfToGrey.imageToPdf();
         } catch (IOException e) {
             throw new RuntimeException(e);
